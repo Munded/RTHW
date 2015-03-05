512c1aa95309a24f9849a4224b4092742fbf2131
Edmund O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-03-05 16:17:23 +0000

    Q5

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+my_name = "Ed O'Brien"
+my_age = 22
+my_height = 193
+my_weight = 97
+my_eyes = "Grey"
+my_teeth = "White"
+my_hair = "Dirty Blonde"
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg"
+puts "He's got #{my_eyes} and #{my_hair} hair."
+puts "Hit teeth are usually #{my_teeth} depending on coffee consumption"
+
+puts "If I add #{my_age}, #{my_height} and #{my_weight}, I get #{my_age + my_height + my_weight}."
